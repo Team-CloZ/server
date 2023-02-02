45041a98ab96eb28ec95ffd036e7d841b57aefac
Me1e <mele0404@naver.com>
2023-02-03 00:46:29 +0900

    feat: init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from '@/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder().setTitle('CloZ API').setVersion('0.1').build(),
+  );
+  const options = {
+    customCss: '.swagger-ui .topbar { display: none }',
+  };
+  SwaggerModule.setup('docs', app, document, options);
+
   await app.listen(3000);
 }
 bootstrap();
