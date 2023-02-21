53f92d2e9914d152e3ddcc050e8f379d0a248a9f
Me1e <mele0404@naver.com>
2023-02-21 16:07:05 +0900

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import customCss from '@/custom.css';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://cloz-cloz.vercel.app', 'http://localhost:3001'],
+    credentials: true,
+  });
 
   const document = SwaggerModule.createDocument(
     app,
